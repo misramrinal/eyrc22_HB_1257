79a465ec550f4db9a3bf941460b7d0f4f84c3dab
sovit1 <99137535+sovit1@users.noreply.github.com>
2023-03-08 19:35:14 +0530

    Update wireless_com_final.ino

--- wireless_com_final.ino
+++ wireless_com_final.ino
@@ -2,16 +2,19 @@
 #include <WiFiUdp.h>
 #include <AccelStepper.h>
 #include <Ticker.h>
+
 Ticker blinker;
- 
+
 // Set WiFi credentials
-#define WIFI_SSID "Mrinal"
-#define WIFI_PASS "mrinal1729"
+#define WIFI_SSID "Sovit"
+#define WIFI_PASS "123123123"
 #define UDP_PORT 4210
-AccelStepper motor1(1,26,25);//D0 AND D1
-AccelStepper motor2(1,14,12);//D2 AND D3
-AccelStepper motor3(1,13,16);//D5 AND D6
-int i,k=0;
+
+AccelStepper motor1(1,16,5);//D0 AND D1
+AccelStepper motor2(1,4,0);//D2 AND D3
+AccelStepper motor3(1,14,12);//D5 AND D6
+
+int i=0;
 int s[3];
 int v1,v2,v3;
 
@@ -20,111 +23,77 @@ WiFiUDP UDP;
 char packet[255];
 char *msg;
  
-void setup() {
-  // Setup serial port
+void setup() 
+{
+  
   Serial.begin(115200);
   Serial.println();
  
   // Begin WiFi
   WiFi.begin(WIFI_SSID, WIFI_PASS);
  
-  // Connecting to WiFi...
-  Serial.print("Connecting to ");
-  Serial.print(WIFI_SSID);
-  // Loop continuously while WiFi is not connected
+  
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(100);
-    Serial.print(".");
+
   }
  
   // Connected to WiFi
-  Serial.println();
-  Serial.print("Connected! IP address: ");
-  Serial.println(WiFi.localIP());
+  
 
   // Begin listening to UDP port
   UDP.begin(UDP_PORT);
-  Serial.print("Listening on UDP port ");
-  Serial.println(UDP_PORT);
+
+  
   motor1.setMaxSpeed(1000.0);
   motor2.setMaxSpeed(1000.0);
   motor3.setMaxSpeed(1000.0);
-  blinker.attach_ms(1,motorControl);
- 
-}
-
-void loop() {
-
-  // If packet received...
-  int packetSize = UDP.parsePacket();
-  if (packetSize) {
-    Serial.print("Received packet! Size: ");
-    Serial.println(packetSize);
-    int len = UDP.read(packet, 255);
-    if (len > 0)
-    {
-      packet[len] = '\0';
-    }
-//    Serial.print("Packet received: ");
-//    Serial.println(packet);
-//    for(i=0;i<255;i++)
-//    {
-////      if(packet[i]==' ')
-////      break;
-//      if(packet[i]==',')
-//      k++;
-//      else
-//      s[k]=s[k]+packet[i];
-//    }
-
-
-msg = strtok(packet,",");
-int i =0;
-while(msg!=NULL)
-{
-  s[i]=atoi(msg);
-  msg=strtok(NULL,",");
-  i++;
-}
-
-
-
-
-
-
-    k=0;
-    v1=s[0];
-    v2=s[1];
-    v3=s[2];
-    
-    
-//    Serial.println(v1);
-//    Serial.println(v2);
-//    Serial.println(v3);
-//    delay(150);
-   
-    motor1.setSpeed(v1);
-    motor2.setSpeed(v2);
-    motor3.setSpeed(v3);
-    //Running the motor
-//    motor1.runSpeed();
-//    motor2.runSpeed();
-//    motor3.runSpeed();
-//    
-
-    // Send return packet
-//    UDP.beginPacket(UDP.remoteIP(), UDP.remotePort());
-//    UDP.write(reply);
-//    UDP.endPacket();
-
-  }
-
+  
+  blinker.attach_ms(1,motorControl); 
 }
 
 void motorControl(void)
 {
+  motor1.setSpeed(v1);
+  motor2.setSpeed(v2);
+  motor3.setSpeed(v3);
+}
+
+void loop() 
+{
+
   motor1.runSpeed();
   motor2.runSpeed();
   motor3.runSpeed();
+
+  // If packet received...
+  int packetSize = UDP.parsePacket();
+ 
+  if (packetSize) 
+  {
+    int len = UDP.read(packet, 255);
+    if (len > 0)
+    {
+      packet[len] = 0;
+    }
+    
+    msg = strtok(packet,",");
+
+    while(msg!=NULL)
+    {
+
+      int x = atoi(msg);
+      s[i++]= int(x);
+      msg=strtok(NULL,",");
+    }
+
+    v1=s[0];
+    v2=s[1];
+    v3=s[2];
+    
+    i = 0;
+
+  }
+
 }
